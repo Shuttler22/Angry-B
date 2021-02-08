c6bb20d556805dbf582fc684e5120a48ba7c6a47
Shuttler22 <76698735+Shuttler22@users.noreply.github.com>
2021-02-08 22:23:22 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,21 @@
+class chain {
+    constructor(bodyA, bodyB){
+        var options={
+            bodyA: bodyA,
+            bodyB: bodyB,
+            stiffness:0.04
+            , length:10
+
+        }
+       this.chain = constraint.create(options);
+       World.add(world, this.chain);
+
+       
+    }
+    display(){
+        var a = this.chain.bodyA.position
+        var b = this.chain.bodyB.position
+        strokeWeight(3);
+        line(a.x, a.y, b.x, b.y)
+    }
+}
